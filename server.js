1535abaa550dfebddade3ccc7f93d648fed38bf7
Alexey <suxovarov.alexey@gmail.com>
2023-03-27 14:32:09 +0300

    add restaurants.route.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express"
 import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/restaurant")
+app.use("/api/v1/restaurant", restaurants)
 app.use("*", (req, res) => res.status(404).json({error: "not found"}))
 
 export default app
